446e2236670b12ffb59db64836102cc372fe9931
Jerson <>
2019-07-30 06:00:23 -0300

    to heroku

--- Startup.cs
+++ Startup.cs
@@ -45,7 +45,7 @@ namespace EventoContabil
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
+            if (env.IsProduction() || env.IsStaging() || env.IsEnvironment("Staging_2"))
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
